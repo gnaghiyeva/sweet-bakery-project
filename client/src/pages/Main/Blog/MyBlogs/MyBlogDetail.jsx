cd419591134148ec9caeff73f0aa30e8ca569a29
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2023-08-04 20:48:48 +0400

    main-blog-detail

--- client/src/pages/Main/Blog/MyBlogs/MyBlogDetail.jsx
+++ client/src/pages/Main/Blog/MyBlogs/MyBlogDetail.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getBlogDetailById } from '../../../../api/requests';
 import { useParams } from 'react-router-dom';
-
+import blogdetailStyle from '../../../../style/blogdetail.module.css'
 const MyBlogDetail = () => {
   const [blogdetails, setBlogDetails] = useState([])
   const { id } = useParams();
@@ -12,14 +12,45 @@ const MyBlogDetail = () => {
   }, [id]);
   return (
     <>
-    <ul>
-      {blogdetails.map((detail)=>(
-    <li>
-      {detail.menuTitle}
-    </li>
+      <section className={blogdetailStyle.blog_detail_container}>
 
-      ))}
-    </ul>
+        {blogdetails && blogdetails.map((detail) => (
+          <>
+            <div>
+              <article>
+                <p className={blogdetailStyle.blog_detail_p}>{detail.description}</p>
+              </article>
+            </div>
+            <br />
+
+            <div>
+
+              <article>
+                <h4 className={blogdetailStyle.blog_detail_menu_title}>{detail.menuTitle}</h4><br/>
+                <p>{detail.menuDesc}</p>
+              </article>
+
+              <br />
+
+              <div className={blogdetailStyle.blog_detail_image_container}>
+                <img className={blogdetailStyle.blog_detail_image} src={detail.image} />
+              </div>
+            </div>
+
+            <br />
+            <div>
+              <article>
+                <h4 className={blogdetailStyle.blog_detail_guest_title}>{detail.guestTitle}</h4><br/>
+                <p>{detail.guestDesc}</p>
+              </article>
+            </div>
+          </>
+        ))}
+
+
+
+
+      </section>
     </>
   )
 }
